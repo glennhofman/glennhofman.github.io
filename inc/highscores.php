61633f8e3baa169e58f5cf3b579bd6809712216b
Glenn Hofman <glennh1991@gmail.com>
2021-03-27 18:37:44 +0100

    added working game Space Runner

---
+++ inc/highscores.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET["name"])){
+  $name = $_GET["name"];
+  $score = $_GET["score"];
+  if($name === ""){
+    $name = "Noname";
+  }
+}
+
+$highscores = json_decode(file_get_contents('highscores.txt'),true);
+if(isset($_GET["name"])){
+$highscores[$name] = $score;
+
+$file = "highscores.txt";
+file_put_contents($file, json_encode($highscores), LOCK_EX);
+}
+arsort($highscores);
+echo json_encode($highscores);
+?>
